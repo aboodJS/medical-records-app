fc8af3140aeeb79cb81c4b8e71ff9595ec119c1e
ajs <140010746+aboodJS@users.noreply.github.com>
2025-12-03 16:22:28 +0300

    initial commit

---
+++ app/layouts/default.vue
@@ -0,0 +1,46 @@
+<template>
+  <main>
+    <nav>
+      <h1>hello world</h1>
+      <ul>
+        <li><button>login</button></li>
+      </ul>
+    </nav>
+    <slot></slot>
+  </main>
+</template>
+
+<style scoped>
+nav {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 95vw;
+  margin-inline: auto;
+}
+
+nav ul {
+  list-style: none;
+  display: flex;
+  align-items: center;
+  margin: 0;
+  gap: 12px;
+}
+
+button {
+  height: 35px;
+  width: 65px;
+  font-size: large;
+  font-weight: bold;
+  border: 0;
+  border-radius: 3px;
+  background-color: cyan;
+  cursor: pointer;
+  transition: 150ms;
+}
+
+button:hover {
+  background-color: darkcyan;
+  color: white;
+}
+</style>
---
+++ app/pages/index.vue
@@ -0,0 +1,3 @@
+<template>
+  <h3>this is a home page</h3>
+</template>
