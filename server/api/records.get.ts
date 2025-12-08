bf43e71a07661770eb1c1f27bc7447c3342b9eab
ajs <140010746+aboodJS@users.noreply.github.com>
2025-12-08 20:57:23 +0300

    [CHANGES] - made some simple API Routes

---
+++ server/api/records.get.ts
@@ -0,0 +1,7 @@
+export default eventHandler((event) => {
+  return [
+    { name: "Ahmed", age: 21, stay: "3 weeks" },
+    { name: "Malek", age: 15, stay: "3 days" },
+    { name: "Hamza", age: 29, stay: "week" },
+  ];
+});
