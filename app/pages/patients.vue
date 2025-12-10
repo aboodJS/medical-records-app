e942ed63cad75ffe3f393e7963e7702286c8a9d2
ajs <140010746+aboodJS@users.noreply.github.com>
2025-12-10 20:23:22 +0300

    added a table

--- app/pages/patients.vue
+++ app/pages/patients.vue
@@ -1,1 +1,26 @@
-<template></template>
+<script lang="ts" setup>
+let { data } = await useFetch("/api/records");
+console.log(data.value);
+</script>
+
+<template>
+  <main class="bg-[#f8fafc]">
+    <h1 class="text-3xl font-bold">Patients</h1>
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Age</th>
+          <th>Stay</th>
+        </tr>
+      </thead>
+      <tbody v-for="patient in data">
+        <tr>
+          <td>{{ patient.name }}</td>
+          <td>{{ patient.age }}</td>
+          <td>{{ patient.stay }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </main>
+</template>
